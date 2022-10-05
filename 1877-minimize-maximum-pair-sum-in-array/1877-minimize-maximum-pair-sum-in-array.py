1cc684c9093f5aa59fff197bc2b410dbcb9e5548
Sohyun(Lenna) Park <94810121+spa-rk@users.noreply.github.com>
2022-10-04 19:03:25 -0500

    Time: 1372 ms (84.77%), Space: 28.5 MB (13.16%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -5,4 +5,6 @@ class Solution:
         for i in range(len(nums)):
             res.append(nums[i] + nums[len(nums)-1-i])
             
+        print(res)
+        print(max(res))
         return max(res)
