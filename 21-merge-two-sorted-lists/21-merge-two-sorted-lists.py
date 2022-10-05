e3fe6567735373516cae7efa3170fc3c1599701b
Sohyun(Lenna) Park <94810121+spa-rk@users.noreply.github.com>
2022-10-04 19:46:25 -0500

    Time: 69 ms (43.31%), Space: 13.8 MB (98.74%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,26 +5,27 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if list1 == None and list2 == None:
-            return None
-        
-        if list1 == None:
+        if not list1:
             return list2
-        if list2 == None:
+        if not list2:
             return list1
         
-        tail = temp = ListNode()
-
-        while list1 != None and list2 != None:                
-            if list1.val > list2.val:
-                tail.next = list2
-                list2 = list2.next
-            else:
-                tail.next = list1
+        dummy = ListNode()
+        curr = dummy
+        
+        while list1 and list2:
+            # sort starting from smallest
+            if list1.val < list2.val:
+                curr.next = list1
                 list1 = list1.next
-            tail = tail.next
-        tail.next = list1 or list2
-
-        return temp.next
+            # if list2 has smallaer value
+            else:
+                curr.next = list2
+                list2 = list2.next
+            # current node points to the next node
+            curr = curr.next
+        # after the loop iteration, only the tail element would be left, so discard(ignore) it
+        curr.next = list1 if list1 else list2
+        # return dummy's next pointer, which is current node(head)
+        return dummy.next
                 
